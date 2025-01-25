0355e696d21c9dd309f984f5b77eb3e94ae518c1
NHN DEV <ngohuunhan2001pro@gmail.com>
2025-01-25 21:42:01 +0700

    Config cors

--- back/index.js
+++ back/index.js
@@ -9,7 +9,18 @@ const connectMongo = require('./connectMongo');
 
 connectMongo();
 
-app.use(cors());
+var allowlist = [process.env.FRONT_APP_URL]
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.use(cors(corsOptionsDelegate));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
